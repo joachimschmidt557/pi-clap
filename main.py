be210a40f8b5adc02641186bfa43a6af2b6dc247
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-06-24 09:33:55 +0800

    WIP

--- main.py
+++ main.py
@@ -4,13 +4,42 @@ import sys
 import _thread
 from time import sleep
 from array import array
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+
+import serial
+import serial.tools.list_ports
 
 clap = 0
 wait = 2
 flag = 0
 pin = 24
-exitFlag = False	
+exitFlag = False
+
+ports = list(serial.tools.list_ports.comports())    #List of serial ports (loaded automatically)
+
+currentlyOn = False
+
+ON_POSITION = 0
+OFF_POSITION = 180
+
+def getCOM():
+    """
+    Function that returns the COM port of the XBee (if available)
+    """
+    #Is list ports empty?
+    if not ports:
+        print("No Serial Ports found! Exiting now")
+        exit()
+
+    #If there is only one port available, automatically use that one
+    if len(ports) == 1:
+        return ports[0].device
+
+    #Display all available ports if there are more than one available
+    print("Available Ports: ")
+    for port in ports:
+        print(port)
+    return input("Enter Xbee Serialport: ")
 
 def toggleLight(c):
 	GPIO.output(c,True)
@@ -28,7 +57,8 @@ def waitForClaps(threadName):
 	sleep(wait)
 	if clap == 2:
 		print("Two claps")
-		toggleLight(pin)
+		#toggleLight(pin)
+		toggleServo()
 	# elif clap == 3:
 	# 	print "Three claps"
 	elif clap == 4:
@@ -55,8 +85,18 @@ def main():
 					input=True,
 					output=True,
 					frames_per_buffer=chunk)
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(pin, GPIO.OUT)
+	#GPIO.setmode(GPIO.BCM)
+	#GPIO.setup(pin, GPIO.OUT)
+
+	#Set up serial
+	try:
+		serialport = getCOM()
+		print("Establishing connection to: %s" % serialport)
+		ser = serial.Serial(serialport, 9600, timeout=1)
+	except:
+		print("Error establishing connection to serial port. Exiting now")
+		exit()
+
 	try:
 		print("Clap detection initialized")
 		while True:
@@ -78,5 +118,13 @@ def main():
 		p.terminate()
 		GPIO.cleanup()
 
-if __name__ == '__main__':
+def toggleServo():
+	if currentlyOn:
+		currentlyOn = False
+		ser.write(bytes[OFF_POSITION])
+	else:
+		currentlyOn = True
+		ser.write(bytes[ON_POSITION])
+
+if __name__ == "__main__":
 	main()

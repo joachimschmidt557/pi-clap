305c86d580d97f6d0f9f99573a80794f1c37fa57
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-07-26 19:26:13 +0800

    Copied Arduino code from Arduino respository

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,24 @@
+# This code originates from the MagnusFlieger/Arduino respository
+# which can be found here: https://github.com/MagnusFlieger/Arduino/
+
+#include <Servo.h>
+Servo myservo;  // create servo object to control a servo
+
+int pos = 90;    // variable to store the servo position
+int Change = 0;
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+  myservo.write(pos);
+}
+
+void loop() {
+  int Read = Serial.read();
+  // When Serial.read() equals -1, then no data has been transferred
+  if (Read != -1) {
+    Serial.print(Read);
+    myservo.write(Read);
+  }
+  delay(100);
+}

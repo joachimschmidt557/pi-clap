4468dc317bfcddb0cb27051b7b81f2af9e14a6d1
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-07-28 16:41:33 +0800

    Testing in new file

---
+++ Test.2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import pyaudio
+#import wave
+
+import threading
+import array
+
+CHUNK = 512
+FORMAT = pyaudio.paInt16 #paInt8
+CHANNELS = 1
+RATE = 44100 #sample rate
+RECORD_SECONDS = 30
+#WAVE_OUTPUT_FILENAME = "pyaudio-output.wav"
+
+data = None
+done = False
+
+def analyze():
+    while not done:
+        dataAsInts = array.array('h', data)
+        maxValue = max(dataAsInts)
+
+
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK) #buffer
+
+print("* recording")
+
+thrd = threading.Thread(target=analyze)
+
+try:
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+    #frames.append(data) # 2 bytes(16 bits) per channel
+except KeyboardInterrupt:
+    done = True
+
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+#wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+#wf.setnchannels(CHANNELS)
+#wf.setsampwidth(p.get_sample_size(FORMAT))
+#wf.setframerate(RATE)
+#wf.writeframes(b''.join(frames))
+#wf.close()
